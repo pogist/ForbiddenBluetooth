d9169c65967b9acb97569de1fac7d41154a5f624
pogist <murilo.paixao.2@gmail.com>
2019-05-30 14:41:09 -0300

    feat: add scanning indicator on device list view

--- ForbiddenBluetooth/DeviceListViewController.swift
+++ ForbiddenBluetooth/DeviceListViewController.swift
@@ -8,4 +8,58 @@
 
 import UIKit
 
-class DeviceListViewController: UITableViewController { }
+class DeviceListViewController: UITableViewController {
+
+  // MARK: UI components
+
+  lazy var scanningLabel: UILabel = {
+    let label = UILabel(frame: .zero)
+
+    label.text = "Scanning..."
+    label.translatesAutoresizingMaskIntoConstraints = false
+
+    return label
+  }()
+
+  lazy var activityIndicator: UIActivityIndicatorView = {
+    let activityIndicator = UIActivityIndicatorView(style: .gray)
+
+    activityIndicator.color = .black
+    activityIndicator.hidesWhenStopped = true
+    activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+
+    activityIndicator.startAnimating()
+
+    return activityIndicator
+  }()
+
+  lazy var scanningHeader: UIStackView = {
+    let headerView = UIStackView(arrangedSubviews: [activityIndicator, scanningLabel])
+
+    headerView.alignment = .center
+    headerView.axis = .horizontal
+    headerView.distribution = .fillProportionally
+    headerView.spacing = 8
+
+    return headerView
+  }()
+
+  // MARK: UIViewController lifecycle
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    setupNavigationBar()
+    setupTableView()
+  }
+
+  func setupNavigationBar() {
+    title = "Bluetooth"
+    navigationController?.navigationBar.prefersLargeTitles = true
+    navigationItem.titleView = scanningHeader
+  }
+
+  func setupTableView() {
+    tableView.tableFooterView = UIView()
+  }
+}
